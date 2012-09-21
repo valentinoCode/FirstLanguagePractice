70d5a34c713f9b2f15093ba481660a563b8618c3
Diego Bogado <emijbuscaglia@hotmail.com>
2012-09-21 13:08:42 -0300

    2da subida

---
+++ CSharpInterpreter/CSharpInterpreter.Library/Language/Expressions/ConstantExpression.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpInterpreter.Library.Language
+{
+    public class ConstantExpression:IExpression
+    {     
+        public object Evaluate(BindingEnvironment environment)
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
---
+++ CSharpInterpreter/CSharpInterpreter.Library/Language/Expressions/IExpression.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpInterpreter.Library.Language
+{
+    public interface IExpression
+    {
+        object Evaluate(BindingEnvironment environment);
+    }
+}
