0d6188e4031e62e659b8454d454653e89423aa26
Diego Bogado <emijbuscaglia@hotmail.com>
2012-09-21 09:38:34 -0300

    primera subida

---
+++ CSharpInterpreter/CSharpInterpreter.Library/Language/BindingEnvironment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpInterpreter.Library.Language
+{
+    public class BindingEnvironment
+    {
+        private Dictionary<string, object> values = new Dictionary<string, object>();
+
+        public object GetValue(string name) 
+        {
+            if (!this.values.ContainsKey(name))            
+                return null;
+            
+            return this.values[name];
+        }
+
+        public void SetValue(string name, object value)
+        {
+            this.values[name] = value;
+        }
+    }
+}
---
+++ CSharpInterpreter/CSharpInterpreter.Test/Language/Expressions/ExpressionsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CSharpInterpreter.Library.Language;
+
+namespace CSharpInterpreter.Test
+{
+    [TestFixture]
+    public class ExpressionsTests
+    {
+        [Test]
+        public void ANotDefinedVariableSholdReturnNull()
+        {
+            BindingEnvironment environment = new BindingEnvironment();
+            //VariableExpres
+        }
+    }
+}
